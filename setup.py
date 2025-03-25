bc526eac3f3b01265ceb322e70e59d2bfaed7906
dany tsan <danytsan23@gmail.Com>
2025-03-25 12:46:37 +0100

    setup with creating class Game and comment each def

--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
 import pygame
 import sys
 
-pygame.init()
-screen_width = 600
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Space Invader') # title
-clock = pygame.time.Clock() #speed in fps
+class Game:
+    # Contains all variables and functions necessary for the smooth gameplay
+    def __init__(self):
+        pass
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+    def run(self):
+        # update and draw all sprite groups
+        pass
 
-    screen.fill((30,30,30)) # fill with rbg color
+if __name__ == '__main__':
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption('Space Invader') # title
+    clock = pygame.time.Clock() #speed in fps
+    game = Game()
 
-    pygame.display.flip()
-    clock.tick(60) # speed with 60 fps
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30,30,30)) # fill with rbg color
+        game.run() # our game logic setup into the loop
+
+        pygame.display.flip()
+        clock.tick(60) # speed with 60 fps
